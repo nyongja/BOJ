351980277703600257f95f865d6a07094bbbc61d
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-06-29 23:52:36 +0900

    14888. 연산자 끼워넣기

--- back_tracking/14888_연산자끼워넣기.py
+++ back_tracking/14888_연산자끼워넣기.py
@@ -1,6 +1,6 @@
 n = int(input())
 array = list(map(int, input().split()))
-operator = list(map(int, input().split())) # + , -, *, /
+op_num = list(map(int, input().split())) # + , -, *, /
 
 max_result = -1 * int(1e9)
 min_result = int(1e9)
@@ -17,38 +17,26 @@ def dfs(x) :
         return
 
     else :
-        if operator[0] != 0 :
-            prev = array[x+1]
-            array[x+1] = array[x] + array[x+1]
-            operator[0] -= 1
-            dfs(x+1)
-            array[x+1] = prev # 해당 케이스틑 다 봤으니 이전 값응로 되돌림(중복 계산 방지)
-            operator[0] += 1
-        if operator[1] != 0 :
-            prev = array[x+1]
-            array[x+1] = array[x] - array[x+1]
-            operator[1] -= 1
-            dfs(x+1)
-            array[x+1] = prev
-            operator[1] += 1
-        if operator[2] != 0 :
-            prev = array[x+1]
-            array[x+1] = array[x] * array[x+1]
-            operator[2] -= 1
-            dfs(x+1)
-            array[x+1] = prev
-            operator[2] += 1
-        if operator[3] != 0 :
-            prev = array[x+1]
-            if array[x] < 0 : # 음수인 경우 -> 양수로 바꾼 후 다시 음수 처리
-                array[x+1] = -(abs(array[x]) // array[x+1])
-            else :
-                array[x+1] =  array[x] // array[x+1] 
-            operator[3] -= 1
-            dfs(x+1)
-            array[x+1] = prev
-            operator[3] += 1
-
+        for idx, op in enumerate(op_num) :
+            if op_num[idx] != 0 :
+                prev = array[x+1]
+                if idx == 0 :
+                    array[x+1] = array[x] + array[x+1]
+                elif idx == 1 :
+                    array[x+1] = array[x] - array[x+1]
+                elif idx == 2 :
+                    array[x+1] = array[x] * array[x+1]
+                else :
+                    prev = array[x+1]
+                    if array[x] < 0 : # 음수인 경우 -> 양수로 바꾼 후 다시 음수 처리
+                        array[x+1] = -(abs(array[x]) // array[x+1])
+                    else :
+                        array[x+1] =  array[x] // array[x+1]
+                op_num[idx] -= 1
+                dfs(x+1)
+                array[x+1] = prev # 해당 케이스틑 다 봤으니 이전 값응로 되돌림(중복 계산 방지)
+                op_num[idx] += 1
+        
 dfs(0)
 print(max_result)
 print(min_result)
