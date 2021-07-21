039707d3e1a2c506790068d0e0aa9316349a5b9f
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-07-21 11:44:33 +0900

    17298. 오큰수**

---
+++ stack/17298_오큰수.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n = int(input())
+seq = list(map(int, input().split()))
+oh_big = [-1] * n
+stack = deque()
+
+for i in range(n) :
+    while stack and (stack[-1][0] < seq[i]) :
+        _, idx = stack.pop()
+        oh_big[idx] = seq[i]
+    stack.append([seq[i], i])
+
+print(*oh_big)
