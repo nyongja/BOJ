98dfe8aacb6a543b729b34ec2819622d4c0a9a9f
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-08-03 00:16:27 +0900

    2261. 가장 가까운 두 점**

--- divide_and_conquer/2261_가장가까운두점.py
+++ divide_and_conquer/2261_가장가까운두점.py
@@ -1,46 +1,46 @@
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
-def dist(a, b) :
-    return ((b[0] - a[0])**2 + (b[1] - a[1])**2)
+n = int(sys.stdin.readline())
+sorted_location = []
+for _ in range(n):
+    x, y = list(map(int, sys.stdin.readline().split()))
+    sorted_location.append((x, y))
+sorted_location.sort()
 
-def divide_and_conquer(start, end):
-    length = end - start
-    if length == 2:
-        return dist(location[start], location[start+1])
-    elif length == 3:
-        return min(dist(location[start], location[start+1]), dist(location[start], location[start+2]), dist(location[start+1], location[start+2]))
-    else: # length > 3
-        mid = (start + end)//2
-        d = min(divide_and_conquer(start, mid), divide_and_conquer(mid, end))
+def get_dist(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+
+def solution(l, r):
+    if l == r:
+        return float('inf')
+    else:
+        m = (l + r) // 2
+        min_dist = min(solution(l, m), solution(m + 1, r))
+        target_list = []
         
-        midX = location[mid][0]
-        cmp_list = []
-        for i in range(start, end):
-            if (location[i][0] - midX)**2 <= d: # d보다 크면 볼 가치가 없음
-                cmp_list.append(location[i])
-        
-        clist_len = len(cmp_list)
-        if clist_len >= 2:
-            # y 좌표로 정렬
-            cmp_list.sort(key = lambda x:x[1])
-            for i in range(clist_len - 1):
-                for j in range(i+1, clist_len):
-                    if (cmp_list[i][1] - cmp_list[j][1])**2 > d:
-                        break
-                    elif cmp_list[i][0] < midX and cmp_list[j][0] < midX:
-                        continue
-                    elif cmp_list[i][0] >= midX and cmp_list[j][0] >= midX:
-                        continue
-                    d = min(d, dist(cmp_list[i], cmp_list[j]))
-        return d
- 
-n = int(input())
-location = list(set(tuple(map(int, input().split())) for _ in range(n)))
-# x 좌표로 우선 정렬
-location.sort(key = lambda x: x[0])
-if n != len(location):
+        for i in range(m, l - 1, -1):
+            if (sorted_location[i][0] - sorted_location[m][0]) ** 2 < min_dist:
+                target_list.append(sorted_location[i])
+            else:
+                break
+
+        for j in range(m + 1, r + 1):
+            if (sorted_location[j][0] - sorted_location[m][0]) ** 2 < min_dist:
+                target_list.append(sorted_location[j])
+            else:
+                break
+                
+        target_list.sort(key=lambda x: x[1])
+        for i in range(len(target_list) - 1):
+            for j in range(i + 1, len(target_list)):
+                if (target_list[i][1] - target_list[j][1]) ** 2 < min_dist:
+                    dist = get_dist(target_list[i], target_list[j])
+                    min_dist = min(min_dist, dist)
+                else:
+                    break
+        return(min_dist)
+
+if len(sorted_location) != len(set(sorted_location)):
     print(0)
 else:
-    print(divide_and_conquer(0, n))
+    print((solution(0, len(sorted_location) - 1)))
