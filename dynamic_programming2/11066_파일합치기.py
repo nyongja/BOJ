5b02e1efed08ff2df3ed4646c4a4ab1cb881902b
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-09-09 14:35:39 +0900

    11066. 파일 합치기***

--- dynamic_programming2/11066_파일합치기.py
+++ dynamic_programming2/11066_파일합치기.py
@@ -15,4 +15,5 @@ for _ in range(T) :
     for i in range(2, K+1) : # 부분 파일의 길이
         for j in range(1, K-i+2) : # 시작점
             dp[j][j+i-1] = min([dp[j][j+k] + dp[j+k+1][j+i-1] for k in range(i-1)]) + (s[j+i-1] - s[j-1])
+            
     print(dp[1][K])
