9560692f49f51945f91b909b1ecf98d04eafbfc4
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-09-22 21:57:31 +0900

    1956. 운동

---
+++ shortest_path/1956_운동.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+v, e = map(int, input().split())
+graph = [[INF]*(v+1) for _ in range(v+1)]
+for _ in range(e) :
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(v+1) :
+    for i in range(v+1) :
+        for j in range(v+1) :
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+ans = INF
+for i in range(1, v+1) :
+    ans = min(graph[i][i], ans)
+print(ans)
