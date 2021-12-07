b28d42b9aa0a3a87fe38e38f6baa03a05a2aced1
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-12-07 22:53:30 +0900

    11659. 구간 합 구하기 4

--- segment_tree/11659_구간합구하기4.py
+++ segment_tree/11659_구간합구하기4.py
@@ -1,11 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 array = list(map(int, input().split()))
-cumsum = [0]
+cumsum = [0 for _ in range(n+1)]
 
-tmp = 0
-for i in range(n) : # 누적 합 구하기
-    tmp += array[i]
-    cumsum.append(tmp)
+for i in range(1, n+1) : # 누적 합 구하기
+    cumsum[i] = cumsum[i-1] + array[i-1]
 
 for _ in range(m) :
     i, j = map(int, input().split())
